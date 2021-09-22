c02ac0c8ffec103f9061a28f2b89ca02b6a320e8
Md Rafi Akhtar <alimdrafi@gmail.com>
2021-09-23 01:06:29 +0530

    created python package 'distributions'

---
+++ oop-in-python/3a_python_package/distributions/__init__.py
@@ -0,0 +1,9 @@
+from .Gaussiandistribution import Gaussian
+
+"""
+Reason for importing above class:
+    - now you can import the class as
+        `from distributions import Gaussian`
+    - otherwise, you had to import like:
+        `from distributions.Gaussian import Gaussian`
+"""
